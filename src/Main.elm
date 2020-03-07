7618667b829eedd674150605bd267271da8361d5
Andrew Moss <andrew@m0ss.dev>
2020-03-07 13:47:35 -0700

    adjusting views

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, img, input, text)
+import Html exposing (Html, button, div, h1, h2, img, input, li, text, ul)
 import Html.Attributes exposing (placeholder, src, value)
 import Html.Events exposing (onClick, onInput)
 import Http exposing (expectJson, header, request)
@@ -54,7 +54,7 @@ type alias Model =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model NotAsked "word", Cmd.none )
+    ( Model NotAsked "", Cmd.none )
 
 
 type Msg
@@ -67,7 +67,7 @@ fetchRhymesData : String -> Cmd Msg
 fetchRhymesData input =
     Http.request
         { method = "GET"
-        , headers = [ header "x-rapidapi-host" "wordsapiv1.p.rapidapi.com", header "x-rapidapi-key" "a7f48b639amsh91af3afe2b70fcfp1d772bjsnc38a2bbeb654" ]
+        , headers = [ header "x-rapidapi-host" "wordsapiv1.p.rapidapi.com", header "x-rapidapi-key" "KEY" ]
         , url = "https://wordsapiv1.p.mashape.com/words/" ++ input ++ "/rhymes"
         , body = Http.emptyBody
         , expect =
@@ -98,47 +98,59 @@ update msg model =
 ---- VIEW ----
 
 
+renderList : List String -> Html msg
+renderList lst =
+    ul []
+        (List.map (\l -> li [] [ text l ]) lst)
+
+
 viewWords : Words -> Html msg
 viewWords rhymesData =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text rhymesData.word ]
+        [ h2 [] [ text (" Words that rhyme with: " ++ rhymesData.word) ]
         , div [] [ viewRhymes rhymesData.rhymes ]
         ]
 
 
 viewRhymes : Rhymes -> Html msg
 viewRhymes posts =
-    div [] (List.map text posts.all)
-
-
-view : Model -> Html Msg
-view model =
-    case model.rhymesData of
-        NotAsked ->
-            asker model
-
-        Loading ->
-            div [] [ text "Loading" ]
-
-        Failure _ ->
-            div [] [ text "Failure" ]
-
-        Success rhymesData ->
-            viewWords rhymesData
+    renderList posts.all
 
 
 asker : Model -> Html Msg
 asker model =
     div []
         [ img [ src "/logo.svg" ] []
+        , h1 [] [ text "Lime " ]
+        , input [ placeholder "Enter a word!", value model.inputWord, onInput UpdateWord ] []
         , button [ onClick GetRhymesData ] [ text "Search" ]
-        , div []
-            [ input [ placeholder "Word", value model.inputWord, onInput UpdateWord ] []
-            , div [] [ text "Loading" ]
-            , div [] [ text model.inputWord ]
-            ]
         ]
+
+
+view : Model -> Html Msg
+view model =
+    case model.rhymesData of
+        NotAsked ->
+            div []
+                [ asker model
+                , text "Remote Data State: Not Asked"
+                ]
+
+        Loading ->
+            div [] [ text "Remote Data State: Loading" ]
+
+        Failure _ ->
+            div []
+                [ asker model
+                , text "Remote Data State: Failure"
+                ]
+
+        Success rhymesData ->
+            div []
+                [ asker model
+                , text "Remote Data State: Success"
+                , viewWords rhymesData
+                ]
 
 
 

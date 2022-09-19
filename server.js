36e2827d60cabbec5f3e07df43f9082bedd41dae
Andrew Moss <andrew@m0ss.dev>
2022-09-19 14:46:01 -0600

    chore: Updating deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var http = require("http");
 const serve = serveStatic(__dirname + "/build");
 
 const server = http.createServer(
-    (onRequest = (req, res) => {
-        serve(req, res, finalHandler(req, res));
-    })
+  (onRequest = (req, res) => {
+    serve(req, res, finalHandler(req, res));
+  })
 );
 
 server.listen(8080, () => {
-    console.log("server running on 8080...")
-})
+  console.log("server running on 8080...");
+});
